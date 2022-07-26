5bfa4e487c34d53e37bcc7391e559fc8cb270ec0
Tysane Norine <tysaneno@un-i1.kzn.21-school.ru>
2022-07-26 20:32:25 +0300

    START

--- src/s21_smartcalc.c
+++ src/s21_smartcalc.c
@@ -1,1 +1,32 @@
 #include "s21_smartcalc.h"
+
+int main() {
+  struct list a = {2}, b = {1}, c = {4};
+  list* start = &a;
+  a.next = &b;
+  b.next = &c;
+  c.next = NULL;
+  printf("%lf\n", a.value);
+  printf("%lf\n", b.value);
+  printf("%lf\n", c.value);
+
+  return 0;
+}
+
+list* initList(double value) {
+  list* tmp;
+  tmp = (list*)malloc(sizeof(list));
+  if (tmp) {
+    tmp->value = value;
+    tmp->next = NULL;
+  }
+  return tmp;
+}
+
+// void push(list** src, double value) {
+//   list* tmp;
+//   tmp = (list*)malloc(sizeof(list));
+//   if (tmp) {
+//     src
+//   }
+// }
--- src/s21_smartcalc.h
+++ src/s21_smartcalc.h
@@ -1,4 +1,43 @@
 #ifndef SRC_S21_SMARTCALC_H_
 #define SRC_S21_SMARTCALC_H_
 
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef enum {
+  VAL = 1,
+  X = 2,
+  BRACKETSOPEN = 3,
+  BRACKETSCLOSE = 4,
+  ADDITION = 5,
+  SUBTRACTION = 6,
+  MULTIPLICATION = 7,
+  DIVISION = 8,
+  POWER = 9,
+  MODULUS = 10,
+  UNARYPLUS = 11,
+  UNARYMINUS = 12,
+  COS = 13,
+  SIN = 14,
+  TAN = 15,
+  ACOS = 16,
+  ASIN = 17,
+  ATAN = 18,
+  SQRT = 19,
+  LN = 20,
+  LOG = 21
+} type_t;
+
+typedef struct {
+  double value;
+  int priority;
+  type_t type;
+  struct list* next;
+} list;
+
+/*__________________LIST_WORK_FUNCTIONS__________________*/
+list* initList(double value);
+
 #endif  // SRC_S21_SMARTCALC_H_
